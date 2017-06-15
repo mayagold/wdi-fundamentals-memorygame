234b1b779b3b0cf27f0ec83a4579d9628aad1afd
Maya Goldstein <mayagold@Mayas-MacBook-Pro.local>
2017-06-15 12:11:38 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,29 @@
 
 var cards = ["queen", "queen", "king", "king"];
-
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne); 
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2 && cardOne === cardTwo) {
-	alert("You found a match!");
-} else if (cardsInPlay.length === 2 && cardOne !== cardTwo) {
-	alert("Sorry, try again.");
+ var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
 }
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length % 2 === 0) {
+		checkForMatch();
+	}
+ }
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+
+
